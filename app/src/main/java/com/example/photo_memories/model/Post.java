9518b1d4c67feb87da1d8861df9a001e2d3dd1cd
alexYamaoka <ryamaoka1@gmail.com>
2020-04-09 19:44:10 -0700

    updated models

--- app/src/main/java/com/example/photo_memories/model/Post.java
+++ app/src/main/java/com/example/photo_memories/model/Post.java
@@ -6,4 +6,57 @@ public class Post
     private String postImage;
     private String description;
     private String date;
+
+    public Post(String postId, String postImage, String description, String date)
+    {
+        setPostId(postId);
+        setPostImage(postImage);
+        setDescription(description);
+        setDate(date);
+    }
+
+    public Post()
+    {
+        this("", "", "", "");
+    }
+
+    public void setPostId(String postId)
+    {
+        this.postId = postId;
+    }
+
+    public void setPostImage(String postImage)
+    {
+        this.postImage = postImage;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public String getPostId()
+    {
+        return postId;
+    }
+
+    public String getPostImage()
+    {
+        return postImage;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
 }
