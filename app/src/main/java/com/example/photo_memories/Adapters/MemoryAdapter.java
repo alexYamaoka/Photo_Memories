b146592b336ada01ea07d85308c4735b24b9c70b
alexYamaoka <ryamaoka1@gmail.com>
2020-04-10 01:37:15 -0700

    got items to appear on recycler view

--- app/src/main/java/com/example/photo_memories/Adapters/MemoryAdapter.java
+++ app/src/main/java/com/example/photo_memories/Adapters/MemoryAdapter.java
@@ -11,7 +11,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.photo_memories.R;
 import com.example.photo_memories.model.Memory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.ViewHolder>
@@ -41,8 +49,9 @@ public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.ViewHolder
         final Memory memory = memoryList.get(position);
 
         holder.title.setText(memory.getTitle());
-        holder.location.setText(memory.getLocation());
+        holder.location.setText("Location: " + memory.getLocation());
         holder.date.setText(memory.getDate());
+
     }
 
     @Override
@@ -66,4 +75,5 @@ public class MemoryAdapter extends RecyclerView.Adapter<MemoryAdapter.ViewHolder
             date = itemView.findViewById(R.id.date);
         }
     }
+
 }
--- app/src/main/java/com/example/photo_memories/MainActivity.java
+++ app/src/main/java/com/example/photo_memories/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.photo_memories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +14,12 @@ import com.example.photo_memories.Adapters.MemoryAdapter;
 import com.example.photo_memories.model.Memory;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,9 +46,18 @@ public class MainActivity extends AppCompatActivity
         recyclerView.setHasFixedSize(true);
 
 
-        memoryList = new ArrayList<>();
+        loadMemories();
+
+        for (Memory m: memoryList)
+        {
+            System.out.println("Title: " + m.getTitle());
+            System.out.println("Location: " + m.getLocation());
+        }
+
+
         memoryAdapter = new MemoryAdapter(this, memoryList);
         recyclerView.setAdapter(memoryAdapter);
+
 
 
         addNewMemory.setOnClickListener(new View.OnClickListener()
@@ -51,6 +67,37 @@ public class MainActivity extends AppCompatActivity
             {
                 Intent intent = new Intent(MainActivity.this, AddNewMemoryActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+
+    private void loadMemories()
+    {
+        memoryList = new ArrayList<>();
+
+        String currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(currentUser).child("Memories");
+
+        reference.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                memoryList.clear();
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren())
+                {
+                    Memory memory = snapshot.getValue(Memory.class);
+                    memoryList.add(memory);
+                }
+
+                memoryAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
             }
         });
     }
