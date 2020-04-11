36d2eb37b5e8380f577efbc8d1240780e01ddfa6
alexYamaoka <ryamaoka1@gmail.com>
2020-04-10 18:13:18 -0700

    created view all activity

---
+++ app/src/main/java/com/example/photo_memories/ViewAllActivity.java
@@ -0,0 +1,17 @@
+package com.example.photo_memories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAllActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all);
+    }
+}
