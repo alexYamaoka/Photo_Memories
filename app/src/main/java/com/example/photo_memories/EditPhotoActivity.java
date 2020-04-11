580c57d3a5cb1976a33ab3a31bd6319c9f02221f
alexYamaoka <ryamaoka1@gmail.com>
2020-04-10 21:26:28 -0700

    finished edit photo

--- app/src/main/java/com/example/photo_memories/EditPhotoActivity.java
+++ app/src/main/java/com/example/photo_memories/EditPhotoActivity.java
@@ -1,16 +1,26 @@
 package com.example.photo_memories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.photo_memories.model.Memory;
+import com.example.photo_memories.model.Post;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class EditPhotoActivity extends AppCompatActivity
 {
@@ -40,11 +50,77 @@ public class EditPhotoActivity extends AppCompatActivity
         close = findViewById(R.id.close);
 
         Intent intent = getIntent();
-        String postId = intent.getStringExtra("postId");
+        final String postId = intent.getStringExtra("postId");
 
         currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         reference = FirebaseDatabase.getInstance().getReference("Users").child(currentUserId).child("Posts").child(postId);
 
+        // when user inputs value to edit profile
+        reference.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                Post post = dataSnapshot.getValue(Post.class);
 
+                date.setText(post.getDate());
+                location.setText(post.getLocation());
+                description.setText(post.getDescription());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+            }
+        });
+
+
+        close.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+
+
+        delete.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v)
+            {
+                FirebaseDatabase.getInstance().getReference("Users").child(currentUserId).child("Posts").child(postId).removeValue();
+                finish();
+                Toast.makeText(EditPhotoActivity.this, "Photo Deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        save.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                updatePost(date.getText().toString(), location.getText().toString(), description.getText().toString());
+                finish();
+            }
+        });
+
+    }
+
+
+    private void updatePost(String date, String location, String description)
+    {
+        HashMap<String, Object> hashMap = new HashMap<>();
+
+        hashMap.put("date", date);
+        hashMap.put("location", location);
+        hashMap.put("description", description);
+
+        reference.updateChildren(hashMap);
     }
 }
--- app/src/main/java/com/example/photo_memories/PostDetailsActivity.java
+++ app/src/main/java/com/example/photo_memories/PostDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.photo_memories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -10,6 +11,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.photo_memories.model.Memory;
+import com.example.photo_memories.model.Post;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PostDetailsActivity extends AppCompatActivity
 {
@@ -38,17 +47,36 @@ public class PostDetailsActivity extends AppCompatActivity
 
 
         Intent intent = getIntent();
-        String postImageAsString = intent.getStringExtra("postImage");
         final String postIdAsString = intent.getStringExtra("postId");
-        String locationAsString = intent.getStringExtra("location");
-        String dateAsString = intent.getStringExtra("date");
-        String descriptionAsString = intent.getStringExtra("description");
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Posts").child(postIdAsString);
+        reference.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                Post post = dataSnapshot.getValue(Post.class);
+
+                if (post != null)
+                {
+                    Glide.with(PostDetailsActivity.this).load(post.getPostImage()).into(postImage);
+                    date.setText(post.getDate());
+                    location.setText("Location: " + post.getLocation());
+                    description.setText(post.getDescription());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+            }
+        });
 
 
-        Glide.with(PostDetailsActivity.this).load(postImageAsString).into(postImage);
-        location.setText(locationAsString);
-        date.setText(dateAsString);
-        description.setText(descriptionAsString);
+
+
+
 
 
 
@@ -67,7 +95,7 @@ public class PostDetailsActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(PostDetailsActivity.this, EditMemoryActivity.class);
+                Intent intent = new Intent(PostDetailsActivity.this, EditPhotoActivity.class);
                 intent.putExtra("postId", postIdAsString);
                 startActivity(intent);
             }
