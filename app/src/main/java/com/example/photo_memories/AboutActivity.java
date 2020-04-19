2448e3a9105d18c0ffe623748ea79471231ca444
alexYamaoka <ryamaoka1@gmail.com>
2020-04-19 13:42:09 -0700

    added webview to private pocily in about

--- app/src/main/java/com/example/photo_memories/AboutActivity.java
+++ app/src/main/java/com/example/photo_memories/AboutActivity.java
@@ -2,15 +2,42 @@ package com.example.photo_memories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity
 {
+
+    private WebView webView;
+    private final String url = "https://www.iubenda.com/privacy-policy/38684895";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        webView = findViewById(R.id.web_view);
+
+        webView.getSettings().setJavaScriptEnabled(true);       // enable javascript
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+                super.onPageStarted(view, url, favicon);
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                super.onPageFinished(view, url);
+            }
+        });
+        webView.loadUrl(url);
     }
 }
